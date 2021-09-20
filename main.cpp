6a1aa21499525919f5e39eec01e76c261a165105
Pavel <pasha.u.polyak@gmail.com>
2021-09-20 21:43:51 +0700

    add sdl lib

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <SDL.h>
+#include <iostream>
+
+int main()
+{
+	if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0)
+	{
+		std::cerr << "Failed to init SDL: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Quit();
+	return 0;
+}
