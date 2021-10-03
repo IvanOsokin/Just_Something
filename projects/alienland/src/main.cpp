2aca4562775eff3cd616a18b18e6653a0fd9c573
Pavel <16599808+KOMMYHAP@users.noreply.github.com>
2021-10-03 22:02:54 +0700

    Добавил стандартные логгеры в проект

--- projects/alienland/src/main.cpp
+++ projects/alienland/src/main.cpp
@@ -1,11 +1,19 @@
 #include "Game.h"
+#include "Log/DebuggerDisplaySink.h"
+#include "Log/FileLogSink.h"
 
 int main(int /*argc*/, char **argv)
 {
 	auto executableName = std::filesystem::path(argv[0]);
 	auto executableDirectory = executableName.parent_path();
 	auto resourcesDirectory = executableDirectory / "resources";
-	
+
+	Core::Log::Instance().AddSink(std::make_shared<Core::DebuggerDisplaySink>());
+
+	auto fileLogSink = std::make_shared<Core::FileLogSink>();
+	fileLogSink->Init(executableDirectory / "Alienland.log");
+	Core::Log::Instance().AddSink(fileLogSink);
+
 	Game game;
 	game.Start(resourcesDirectory);
 	return 0;
