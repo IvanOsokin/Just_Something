4260c21b55823cebd39e9537f84f73b1898a5b7f
Pavel <16599808+KOMMYHAP@users.noreply.github.com>
2021-10-09 20:27:50 +0700

    Добавил Verify ассерты

--- projects/libcore/include/Asserts/AssertMacros.h
+++ projects/libcore/include/Asserts/AssertMacros.h
@@ -3,13 +3,27 @@
 #include "Asserts.h"
 
 #define Assert(expression) \
-	CORE_ASSERT_IMPL((expression), "Assertion failed.")
+	CORE_ASSERT_IMPL(expression, "Assertion failed.")
 
 #define Assert2(expression, message) \
-	CORE_ASSERT_IMPL((expression), message)
+	CORE_ASSERT_IMPL(expression, message)
+
+#define Verify(expression) \
+	CORE_VERIFY_IMPL(expression, "Expession failed.")
+
+#define Verify2(expession, message) \
+	CORE_VERIFY_IMPL(expession, message)
+
+#define CORE_VERIFY_IMPL(expession, message) \
+	[value = !!(expession)]() -> bool { \
+		Core::HandleAssertion(message, __LINE__, __FILE__); \
+		return value; \
+	}()
 
 #define CORE_ASSERT_IMPL(expression, message) \
-	if (!(expression)) \
-	{ \
-		Core::HandleAssertion(message, __LINE__, __FILE__); \
-	}
+	do {\
+		if (!(expression)) \
+		{ \
+			Core::HandleAssertion(message, __LINE__, __FILE__); \
+		} \
+	} while (false)
