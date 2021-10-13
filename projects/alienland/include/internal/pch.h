542934c0fc226c6b97b10f4a08f612447ca1dd85
Pavel <16599808+KOMMYHAP@users.noreply.github.com>
2021-10-13 20:51:00 +0700

    Вынес pch со стандартными хедерами
    - вынес в stdpch в corelib
    - подключил stdpch в Alienland

--- projects/alienland/include/internal/pch.h
+++ projects/alienland/include/internal/pch.h
@@ -5,31 +5,5 @@
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
 
-#include <string>
-#include <string_view>
-
-#include <bitset>
-#include <vector>
-#include <queue>
-#include <array>
-#include <map>
-#include <set>
-#include <forward_list>
-#include <list>
-#include <deque>
-
-#include <cstdint>
-#include <utility>
-#include <optional>
-#include <memory>
-#include <algorithm>
-#include <random>
-#include <functional>
-
-#include <atomic>
-#include <mutex>
-#include <thread>
-#include <chrono>
-#include <filesystem>
-
+#include "Internal/stdpch.h"
 #include "Core.h"
