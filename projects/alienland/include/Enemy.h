f244e2fcae2989fa5e7447c68545ddccd9c30f5d
IvanOsokin <osokin.ivan.nstu@gmail.com>
2021-10-14 21:06:46 +0700

    Добавил загрузку текстуры для enemy

--- projects/alienland/include/Enemy.h
+++ projects/alienland/include/Enemy.h
@@ -3,14 +3,21 @@
 class Enemy
 {
 public:
-	void Init();
+	void			Init(const std::filesystem::path& resourcesDirectory, std::shared_ptr<sf::RenderWindow> window);
 
-	void ProcessInput(const sf::Event & event);
-	void Update(sf::Time elapsedTime);
-	void Render(sf::RenderTarget & renderTarget);
+	void			ProcessInput(const sf::Event & event);
+	void			Update(sf::Time elapsedTime);
+	void			Render(sf::RenderTarget & renderTarget);
+
+	sf::Sprite&		GetSprite() { return _sprite; }
 
 private:
-	sf::Texture		_texture;
-	sf::Vector2f	_pos;
-	sf::Vector2f	_speed;
+	bool			LoadTexture(const std::string & alienTexturePath);
+
+	sf::Sprite					_sprite;
+	sf::Texture					_texture;
+	sf::Vector2f				_pos;
+	sf::Vector2f				_speed;
+	sf::Vector2f				_unitSpeedVector;
+	float						_baseSpeed = 3000000.0f;
 };
--- projects/alienland/src/Enemy.cpp
+++ projects/alienland/src/Enemy.cpp
@@ -1,18 +1,39 @@
 #include "Enemy.h"
 
-void Enemy::Init()
+void Enemy::Init(const std::filesystem::path & resourcesDirectory, std::shared_ptr<sf::RenderWindow> window)
 {
+	Assert(window);
+
+	const std::string enemyTextureName = "enemy-1.png";
+	auto enemyTexturePath = resourcesDirectory / enemyTextureName;
+
+	std::string enemyTexturePathStr = enemyTexturePath.generic_string();
+
+	LoadTexture(enemyTexturePathStr);
 }
 
-void Enemy::ProcessInput(const sf::Event& /*event*/)
-{
-}
+//void Enemy::ProcessInput(const sf::Event& /*event*/)
+//{
+//}
 
 void Enemy::Update(sf::Time /*elapsedTime*/)
 {
 }
 
-void Enemy::Render(sf::RenderTarget & /*renderTarget*/)
+void Enemy::Render(sf::RenderTarget & renderTarget)
 {
-	
+	renderTarget.draw(_sprite);
+}
+
+bool Enemy::LoadTexture(const std::string& enemyTexturePath)
+{
+	if (!_texture.loadFromFile(enemyTexturePath))
+	{
+		LOG_ERROR() << "Failed to load the enemy's texture.";
+		return false;
+	}
+
+	_sprite.setTexture(_texture);
+	LOG_INFO() << "The enemy's texture was loaded.";
+	return true;
 }
