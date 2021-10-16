0ac8d217ac13fcbe638ff2f22d97b90407a68953
IvanOsokin <osokin.ivan.nstu@gmail.com>
2021-10-17 01:27:40 +0700

    Добавил вычисление базового вектора при движении по диагонали.

--- projects/alienland/src/Character.cpp
+++ projects/alienland/src/Character.cpp
@@ -18,18 +18,24 @@ void Character::ProcessInput(const sf::Event& /*event*/)
 	_unitSpeedVector.x = 0;
 	_unitSpeedVector.y = 0;
 	
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) { _unitSpeedVector.y = -1; }
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) { _unitSpeedVector.y =  1; }
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) { _unitSpeedVector.x = -1; }
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) { _unitSpeedVector.x =  1; }
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) { _unitSpeedVector.y -= 1; }
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) { _unitSpeedVector.y += 1; }
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) { _unitSpeedVector.x -= 1; }
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) { _unitSpeedVector.x += 1; }
+
+	if (_unitSpeedVector.x != 0 && _unitSpeedVector.y != 0)				// Ётот блок изначально был добавлен в метод Update.
+	{																	// ѕри нажатии двух клавиш происходила задержка.
+		const float decreasingCoef = static_cast<float>(pow(2, 0.5));	// Ћоги показывали, что идет посто€нное уменьшение _unitSpeedVector
+		_unitSpeedVector /= decreasingCoef;								// “.е. ProcessInput не вызываетс€ и _unitSpeedVector не обнул€етс€ 
+	}																	// и не происходит переопределение базового вектора
 }
 
 void Character::Update(sf::Time elapsedTime)
 {
-	_sprite.move(sf::Vector2f(_baseSpeed * _unitSpeedVector.x, _baseSpeed * _unitSpeedVector.y) * elapsedTime.asSeconds());
+	_sprite.move(sf::Vector2f(_baseSpeed * _unitSpeedVector) * elapsedTime.asSeconds());
 }
 
-void Character::Render(sf::RenderTarget & renderTarget)
+void Character::Render(sf::RenderTarget& renderTarget)
 {
 	renderTarget.draw(_sprite);
 }
