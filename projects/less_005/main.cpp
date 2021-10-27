43c30fa4ae44b85fe972723b9f16a35cec94dbbe
IvanOsokin <osokin.ivan.nstu@gmail.com>
2021-10-27 20:16:04 +0700

    Изучение наследования ч.2 (Trello)

---
+++ projects/less_005/main.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <vector>
+
+class Animal
+{
+protected:
+	Animal(int value)
+		: _value(value)
+	{}
+
+	int GetValue() const { return _value; }
+
+public:
+	virtual void MakeSound() { std::cout << "There is no sound" << std::endl; }
+	virtual void Print() { std::cout << "This is not what you'd like to see" << std::endl; }
+
+private:
+	int _value;
+};
+
+class Cat : public Animal
+{
+public:
+	Cat(int value)
+		: Animal(value)
+	{}
+
+	virtual void MakeSound() { std::cout << "\"Meow\"" << std::endl; }
+	virtual void Print() { std::cout << "Cat's value is " << GetValue() << ". The cat says "; }
+};
+
+class Pig : public Animal
+{
+public:
+	Pig(int value)
+		: Animal(value)
+	{}
+
+	virtual void MakeSound() { std::cout << "\"Oink\"" << std::endl; };
+	virtual void Print() { std::cout << "Pig's value is " << GetValue() << ". The pig says "; }
+};
+
+class Fox : public Animal
+{
+public:
+	Fox(int value)
+		: Animal(value)
+	{}
+
+	virtual void MakeSound() { std::cout << "\"Yipping\"" << std::endl; };
+	virtual void Print() { std::cout << "Fox's value is " << GetValue() << ". The fox says "; }
+};
+
+int main()
+{
+	Cat cat(1);
+	Animal& rCat = cat;
+	rCat.Print();
+	rCat.MakeSound();
+
+	Pig pig(2);
+	Animal* pPig = &pig;
+	pPig->Print();
+	pPig->MakeSound();
+
+	Fox fox(3);
+	Animal& rFox = fox;
+	rFox.Print();
+	rFox.MakeSound();
+
+	std::cout << std::endl << "////////////////Array of pointers to animals////////////////" << std::endl;
+
+	Animal* arr[] = { &cat, &pig, &fox };
+
+	//for (int i = 0; i < 3; ++i)
+	//{
+	//	arr[i]->Print();
+	//	arr[i]->MakeSound();
+	//}
+	for (const auto& it : arr)
+	{
+		it->Print();
+		it->MakeSound();
+	}
+
+	std::cout << std::endl << "////////////////Vector of pointers to animals////////////////" << std::endl;
+
+	std::vector<Animal*> aniVec;
+	aniVec.emplace_back(&cat);
+	aniVec.emplace_back(&fox);
+	aniVec.emplace_back(&pig);
+
+	//std::cout << std::endl << "////////////////Vector of pointers to animals////////////////" << std::endl;
+	//for (size_t i = 0; i < aniVec.size(); i++)
+	//{
+	//	aniVec[i]->MakeSound();
+	//}
+
+	//std::cout << std::endl << "////////////////Vector of pointers to animals////////////////" << std::endl;
+	//for (auto it = aniVec.begin(); it != aniVec.end(); ++it)
+	//{
+	//	(*it)->MakeSound();
+	//}
+
+	for (const auto& it : aniVec)
+	{
+		it->Print();
+		it->MakeSound();
+	}
+
+	std::cout << std::endl << "////////////////Array of animals////////////////" << std::endl;
+
+	Animal arr2[] = { Cat(11), Pig(22), Fox(33) };
+
+	for (auto& it2 : arr2)
+	{
+		it2.Print();
+		it2.MakeSound();
+	}
+
+	return 0;
+}
