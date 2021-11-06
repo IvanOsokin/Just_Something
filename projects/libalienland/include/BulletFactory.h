fc2e2e0c48fdbacb6b074ba818f1ef978d889742
IvanOsokin <osokin.ivan.nstu@gmail.com>
2021-11-06 15:50:31 +0700

    Добавил вычисление начального положения при создании пули

--- projects/libalienland/include/BulletFactory.h
+++ projects/libalienland/include/BulletFactory.h
@@ -1,21 +1,28 @@
 #pragma once
 
 // Класс BulletFactory:
-// 1. Загружает и хранит все текстуры пуль;
-// 2. Возвращает спрайт пули, соответсвующий определенным условиям, переданным в параметры
+// 1. Загружает и хранит:
+//   - все текстуры пуль;
+//   - базовые расстояния от центра текстуры персонажа до цончика оружия в зависимости от типа оружия
+// 2. Возвращает спрайт пули в зависимости от типа оружия
+// 3. Возвращает начальное положение спрайта пули в зависимости от типа оружия
 // 
 // При неудаче загрузки текстуры:
-//	- загружаемая текстура инициализируется пустой текстурой текстурой
+//	- загружаемая текстура инициализируется пустой текстурой
 //  - в log-файл выводится сообщение об ошибке с указанием имени незагруженной текстуры
 //  - фиксируется ошибка загрузки текстуры
 class BulletFactory
 {
 public:
-	void		Init(const std::filesystem::path & resourcesDirectory, const std::vector<std::string>& bulletTextureTitle);
-	sf::Sprite	GetSprite(/*Some conditions*/);
+	void		 Init(const std::filesystem::path & resourcesDirectory,
+					  const std::vector<std::string> & bulletTextureTitle,
+					  const std::vector<float> & distanceToWeaponTip);
+	sf::Sprite	 GetSprite(/*Depending on WeaponID*/);
+	sf::Vector2f GetInitPosition(/*Depending on WeaponID,*/ const sf::Vector2f & characterPos, const float & angle);
 
 private:
 	void		LoadTextures(const std::vector<std::string> & bulletsTexturesPath);
 
-	std::vector<sf::Texture> _textures;
+	std::vector<sf::Texture>	_textures;
+	std::vector<float>			_distanceToWeaponTip;		// Distance between the center of the character sprite and the tip of the weapon
 };
--- projects/libalienland/src/BulletFactory.cpp
+++ projects/libalienland/src/BulletFactory.cpp
@@ -1,8 +1,11 @@
 #include "BulletFactory.h"
 
 void BulletFactory::Init(const std::filesystem::path & resourcesDirectory,
-						 const std::vector<std::string> & bulletTextureTitle)
+						 const std::vector<std::string> & bulletTextureTitle,
+						 const std::vector<float> & distanceToWeaponTip)
 {
+	_distanceToWeaponTip = distanceToWeaponTip;
+
 	std::vector<std::string> bulletTexturePath;
 	bulletTexturePath.reserve(bulletTextureTitle.size());
 
@@ -15,12 +18,24 @@ void BulletFactory::Init(const std::filesystem::path & resourcesDirectory,
 	LoadTextures(bulletTexturePath);
 }
 
-sf::Sprite BulletFactory::GetSprite(/*Some conditions*/)
+sf::Sprite BulletFactory::GetSprite(/*Depending on WeaponID*/)
 {
 	sf::Sprite bulletSprite;
 	bulletSprite.setTexture(*_textures.begin());
 
 	return bulletSprite;
+}
+
+sf::Vector2f BulletFactory::GetInitPosition(/*Depending on WeaponID,*/ const sf::Vector2f & characterPos, const float & angle)
+{
+	const float pi = 3.141593f;
+	static const float s_fromRadToDeg = 180.0f / pi;
+
+	sf::Vector2f tipWeaponPosition;
+	tipWeaponPosition.x = (*_distanceToWeaponTip.begin()) * std::cosf(angle) * s_fromRadToDeg;
+	tipWeaponPosition.y = (*_distanceToWeaponTip.begin()) * std::sinf(angle) * s_fromRadToDeg;
+
+	return characterPos + tipWeaponPosition;
 }
 
 void BulletFactory::LoadTextures(const std::vector<std::string> & bulletsTexturesPath)
