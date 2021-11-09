a6093c903a830b87a0a2460f99a9707035821d96
IvanOsokin <osokin.ivan.nstu@gmail.com>
2021-11-09 22:20:35 +0700

    Добавил добавление пули, удаление; обработку коллизий с границами сцены

--- projects/libalienland/include/BulletManager.h
+++ projects/libalienland/include/BulletManager.h
@@ -1,22 +1,37 @@
 #pragma once
 
-class TestScene;
 class Enemy;
+class Bullet;
 class BulletFactory;
 
 class BulletManager
 {
 public:
 	BulletManager();
+	~BulletManager();
 
 	void Init(const std::filesystem::path & resourcesDirectory,
 			  const sf::IntRect & sceneBorder,
 			  std::shared_ptr<Enemy> enemy);
 
+	void AddBullet(const sf::Vector2f & currentCharacterPos, const float & currentCharacterRot, const sf::Vector2f & targetPos);
+
+	void Update (const sf::Time & elapsedTime);
+	void Render (sf::RenderTarget & renderTarget);
+
 private:
-	std::vector<std::string> FillBulletTextureTitle() const;
+	void						ProcessCollision();
+	sf::Vector2f				CalcInitBulletPos(const sf::Vector2f & currentCharacterPos,
+												  const float & currentCharacterRot,
+												  const float & distToWeaponTip);
+	void						SceneBorderCollision();
+	void						EnemyCollision();
+
+	std::vector<std::string>	FillBulletTextureTitle() const;
+	std::vector<float>			DistanceToWeaponTip() const;
 
 	std::shared_ptr<BulletFactory>  _bulletFactory;
 	std::shared_ptr<Enemy>			_enemy;
+	std::vector<Bullet>				_bullets;
 	sf::IntRect						_sceneBorder;
 };
--- projects/libalienland/src/BulletManager.cpp
+++ projects/libalienland/src/BulletManager.cpp
@@ -1,11 +1,14 @@
 #include "BulletManager.h"
 
 #include "BulletFactory.h"
-#include "TestScene.h"
+#include "Bullet.h"
+#include "Enemy.h"
 
 BulletManager::BulletManager()
 	: _bulletFactory(std::make_shared<BulletFactory>())
 {}
+
+BulletManager::~BulletManager() = default;
 
 void BulletManager::Init(const std::filesystem::path & resourcesDirectory,
 						 const sf::IntRect & sceneBorder,
@@ -13,7 +16,75 @@ void BulletManager::Init(const std::filesystem::path & resourcesDirectory,
 {
 	_sceneBorder = sceneBorder;
 	_enemy = enemy;
-	_bulletFactory->Init(resourcesDirectory, FillBulletTextureTitle());
+	_bulletFactory->Init(resourcesDirectory, FillBulletTextureTitle(), DistanceToWeaponTip());
+}
+
+void BulletManager::AddBullet(const sf::Vector2f & currentCharacterPos, const float & currentCharacterRot, const sf::Vector2f & targetPos)
+{
+	sf::Vector2f initBulletPos = CalcInitBulletPos(currentCharacterPos, currentCharacterRot, _bulletFactory->GetInitPosition(/*WeaponID*/));
+
+	// Make bullet speed storage
+	Bullet bullet(2000.0f, _bulletFactory->GetSprite());
+
+	bullet.Init(initBulletPos, targetPos);
+
+	_bullets.emplace_back(bullet);
+}
+
+void BulletManager::Update(const sf::Time & elapsedTime)
+{
+	for (auto & bullet : _bullets)
+	{
+		bullet.Update(elapsedTime);
+	}
+
+	ProcessCollision();
+
+	//LOG_DEBUG() << "_bullets.size() = " << _bullets.size();
+}
+
+void BulletManager::Render(sf::RenderTarget & renderTarget)
+{
+	for (auto & bullet : _bullets)
+	{
+		bullet.Render(renderTarget);
+	}
+}
+
+void BulletManager::ProcessCollision()
+{
+	SceneBorderCollision();
+	EnemyCollision();
+}
+
+void BulletManager::SceneBorderCollision()
+{
+	_bullets.erase(std::remove_if(_bullets.begin(),
+								  _bullets.end(),
+								  [*this](const Bullet & bullet)
+								  {
+				   					  return !_sceneBorder.contains(static_cast<int>(bullet.GetBulletBottomPosition().x),
+				   													static_cast<int>(bullet.GetBulletBottomPosition().y));}),
+				  _bullets.end());
+}
+
+void BulletManager::EnemyCollision()
+{
+
+}
+
+sf::Vector2f BulletManager::CalcInitBulletPos(const sf::Vector2f & currentCharacterPos,
+											  const float & currentCharacterRot,
+											  const float & distToWeaponTip)
+{
+	const float pi = 3.141593f;
+	static const float s_fromDegToRad = pi / 180.0f;
+
+	sf::Vector2f tipWeaponPosition;
+	tipWeaponPosition.x = distToWeaponTip * std::cosf(currentCharacterRot * s_fromDegToRad);
+	tipWeaponPosition.y = distToWeaponTip * std::sinf(currentCharacterRot * s_fromDegToRad);
+
+	return currentCharacterPos + tipWeaponPosition;
 }
 
 std::vector<std::string> BulletManager::FillBulletTextureTitle() const
@@ -24,4 +95,14 @@ std::vector<std::string> BulletManager::FillBulletTextureTitle() const
 	bulletTextureTitle.emplace_back("bullet-1.png");
 
 	return bulletTextureTitle;
+}
+
+std::vector<float> BulletManager::DistanceToWeaponTip() const
+{
+	std::vector<float> distBetweenCharacterCenterAndWeaponTip;
+
+	distBetweenCharacterCenterAndWeaponTip.emplace_back(122.33f);
+	distBetweenCharacterCenterAndWeaponTip.emplace_back(122.33f);
+
+	return distBetweenCharacterCenterAndWeaponTip;
 }
