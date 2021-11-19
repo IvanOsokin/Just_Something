8c45e7ff35748f3d505f070abfb339fdfc76dbdc
Pavel <16599808+KOMMYHAP@users.noreply.github.com>
2021-11-19 16:50:37 +0700

    Вынес corelib в сабмодуль

--- projects/libcore/include/Core.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "Log/LogMacros.h"
-#include "Asserts/AssertMacros.h"
--- projects/libcore/include/Log/LogUtils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "LogMessageData.h"
-
-namespace Core
-{
-	std::ostream & operator<<(std::ostream & ostream, const LogMessageData & messageData);
-	std::ostream & operator<<(std::ostream & ostream, LogMessageSeverity severity);
-}
