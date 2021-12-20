de3fa283f2f7a91bb56f2c1d3fbc840273191e79
IvanOsokin <osokin.ivan.nstu@gmail.com>
2021-12-20 23:35:21 +0700

    Закинул pi в namespace Utils.

--- projects/libalienland/include/Utils.h
+++ projects/libalienland/include/Utils.h
@@ -1,9 +1,9 @@
 #pragma once
 
-const float pi = 3.141593f;
-
 namespace Utils
 {
+	const float pi = 3.141593f;
+
 	float DegreesToRadians(float degrees);
 	float RadiansToDegrees(float radians);
 	float VectorLength(const sf::Vector2f & vector);
