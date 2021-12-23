1bc740dadfb629b327b574fa51f785aca6df87ef
IvanOsokin <osokin.ivan.nstu@gmail.com>
2021-12-23 21:24:57 +0700

    Свел одну функцию в другую. Заменил название типа в шаблоне

--- projects/libalienland/include/SfmlUtils.h
+++ projects/libalienland/include/SfmlUtils.h
@@ -3,14 +3,14 @@
 namespace Utils
 {
 	template<typename T1, typename T2>
-	sf::Vector2<T1> VectorCast(const T2 & vector)
-	{
-		return sf::Vector2<T1>(static_cast<T1>(vector.x), static_cast<T1>(vector.y));
-	}
-
-	template<typename T1, typename T2>
 	sf::Vector2<T1> VectorCast(T2 x, T2 y)
 	{
 		return sf::Vector2<T1>(static_cast<T1>(x), static_cast<T1>(y));
+	}
+
+	template<typename T, typename Vector>
+	sf::Vector2<T> VectorCast(const Vector & vector)
+	{
+		return VectorCast<T>(vector.x, vector.y);
 	}
 }
