148eff96ad7d145c83f599d494b0ec9cc0998271
IvanOsokin <osokin.ivan.nstu@gmail.com>
2022-03-16 23:53:28 +0700

    Стенду передается готовый для испытаний дв-ль

--- projects/lessons/less_007_test/main.cpp
+++ projects/lessons/less_007_test/main.cpp
@@ -11,12 +11,14 @@ int main(int argc, char** argv)
 	auto InitializingFilePath = executableName.parent_path();
 	InitializingFilePath /= "EngineParameters.ini";
 
-
 	UserInterface ui;
 	ui.ProcessInput();
 
+	auto ice = std::make_shared<InternalCombustionEngine>();
+	ice->Init(ui.GetEnvironmentTemperature(), InitializingFilePath);
+
 	TestStand testStand;
-	testStand.Start(std::make_shared<InternalCombustionEngine>(), ui.GetEnvironmentTemperature(), InitializingFilePath);
+	testStand.Start(ice);
 
 	return 0;
 }
