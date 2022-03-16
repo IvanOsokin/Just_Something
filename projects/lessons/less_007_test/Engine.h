21d0ba6e3694108d36468a1ba3b10149462c9de6
IvanOsokin <osokin.ivan.nstu@gmail.com>
2022-03-16 23:53:47 +0700

    Упростил базовый класс

--- projects/lessons/less_007_test/Engine.h
+++ projects/lessons/less_007_test/Engine.h
@@ -7,7 +7,7 @@ class Engine
 public:
 	virtual ~Engine() = default;
 
-	virtual void Start(double environmentTemp, const std::filesystem::path& InitializingFilePath) {}
+	virtual void Start() {}
 	virtual void MainCycle(double time) {}
 	virtual bool IsOverheated() const { return true; }
 };
