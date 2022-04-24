17e1efb0006aa373f45c3ce8b0fc918e9251bffe
Polyakov Pavel <16599808+KOMMYHAP@users.noreply.github.com>
2022-04-24 17:48:06 +0700

    Пример ООП подхода

--- projects/libalienland/include/BulletManager.h
+++ projects/libalienland/include/BulletManager.h
@@ -26,3 +26,116 @@ private:
 
 	std::shared_ptr<BulletFactory>		_bulletFactory;
 };
+
+// #include "Character.h"
+// #include "Enemy.h"
+
+// class Spawner;
+//
+// class World
+// {
+// public:
+// 	void Update();
+// 	void SetSpawner(std::unique_ptr<Spawner> spawner)
+// 	{
+// 		_spawner = std::move(spawner);
+// 	}
+//
+// private:
+// 	std::unique_ptr<Spawner> _spawner;
+// 	std::vector<std::shared_ptr<GameObject>> _gameObjects;
+// };
+//
+// class WorldFactory
+// {
+// public:
+// 	virtual ~WorldFactory() = default;
+// 	virtual std::shared_ptr<World> Create() = 0;
+// };
+//
+// class GameObject;
+// class Spawner
+// {
+// public:
+// 	virtual ~Spawner() = default;
+// 	virtual std::vector<std::shared_ptr<GameObject>> Spawn() = 0;
+// };
+//
+// class TestEnemySpawner : public Spawner
+// {
+// public:
+// 	std::vector<std::shared_ptr<GameObject>> Spawn() override
+// 	{
+// 		if (_spawned)
+// 		{
+// 			return {};
+// 		}
+// 		_spawned = true;
+// 		return {
+// 			std::make_shared<Enemy>()
+// 		};
+// 	}
+//
+// private:
+// 	bool		_spawned = false;
+// };
+//
+// class TestCharacterSpawner : public Spawner
+// {
+// public:
+// 	std::vector<std::shared_ptr<GameObject>> Spawn() override
+// 	{
+// 		if (_spawned)
+// 		{
+// 			return {};
+// 		}
+// 		_spawned = true;
+// 		return {
+// 			std::make_shared<Character>()
+// 		};
+// 	}
+//
+// private:
+// 	bool		_spawned = false;
+// };
+//
+// class TestSpawner : public Spawner
+// {
+// public:
+// 	TestSpawner(std::unique_ptr<Spawner> playerSpawner, std::unique_ptr<Spawner> enemiesSpawner);
+// 	std::vector<std::shared_ptr<GameObject>> Spawn() override
+// 	{
+// 		auto players = _playerSpawner->Spawn();
+// 		auto enemies = _enemiesSpawner->Spawn();
+// 		
+// 		std::vector<std::shared_ptr<GameObject>> allObjects;
+// 		std::move(std::make_move_iterator(players.begin()), std::make_move_iterator(players.end()), std::back_inserter(allObjects));
+// 		std::move(std::make_move_iterator(enemies.begin()), std::make_move_iterator(enemies.end()), std::back_inserter(allObjects));
+// 		return allObjects;
+// 	}
+//
+// 	std::unique_ptr<Spawner> _playerSpawner;
+// 	std::unique_ptr<Spawner> _enemiesSpawner;
+// };
+//
+// inline void World::Update()
+// {
+// 	auto spawnedEntities = _spawner->Spawn();
+// 	std::move(std::make_move_iterator(spawnedEntities.begin()), std::make_move_iterator(spawnedEntities.end()), std::back_inserter(_gameObjects));
+// }
+//
+// class TestWorldFactory : public WorldFactory
+// {
+// public:
+// 	std::shared_ptr<World> Create() override
+// 	{
+// 		auto world = std::make_shared<World>();
+//
+// 		auto playerSpawner = std::make_unique<TestCharacterSpawner>();
+// 		auto enemySpawner = std::make_unique<TestEnemySpawner>();
+// 		
+// 		auto allSpawner = std::make_unique<TestSpawner>(std::move(playerSpawner), std::move(enemySpawner));
+// 		world->SetSpawner(std::move(allSpawner));
+// 		return world;
+// 	}
+// };
