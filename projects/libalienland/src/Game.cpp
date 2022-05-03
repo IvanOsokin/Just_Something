877c678952b83c3ab585c06bdc2070297d0f8d2d
IvanOsokin <osokin.ivan.nstu@gmail.com>
2022-05-03 18:09:28 +0700

    Updated code for Game class based on SfmlGameLoopDelegate

--- projects/libalienland/src/Game.cpp
+++ projects/libalienland/src/Game.cpp
@@ -1,88 +1,23 @@
 #include "Game.h"
+#include "SfmlGameLoopDelegate.h"
 
-#include "GameScene.h"
-
-Game::Game()
-	: _gameScene(std::make_shared<GameScene>())
+Game::Game(std::unique_ptr<GameLoopDelegate> delegate)
+	: _delegate(std::move(delegate))
 {
 }
 
 Game::~Game() = default;
 
-void Game::Start(const std::filesystem::path & resourcesDirectory)
+void Game::Start()
 {
-	AssertVar std::error_code ec;
-	Assert(std::filesystem::is_directory(resourcesDirectory, ec));
-
-	CreateWindow();
-	CreateGameScene(resourcesDirectory);
-
-	PreGameLoop();
+	_delegate->PreGameLoop();
 	GameLoop();
-	PostGameLoop();
-}
-
-void Game::CreateWindow()
-{
-	const std::string gameTitle = "Alien Land";
-	_window = std::make_shared<sf::RenderWindow>(sf::VideoMode(800, 600), gameTitle);
-}
-
-void Game::CreateGameScene(const std::filesystem::path & resourcesDirectory)
-{
-	if (!Verify2(_window, "Window must be created first."))
-	{
-		_shouldTerminate = true;
-		return;
-	}
-	_gameScene->Init(_window, resourcesDirectory);
-}
-
-void Game::PreGameLoop()
-{
-	_gameScene->PreGameLoop();
-}
-
-void Game::PostGameLoop()
-{
-	_gameScene->PostGameLoop();
-}
-
-void Game::PreFrame()
-{
-	_gameScene->PreFrame();
-}
-
-void Game::HandleInput()
-{
-	sf::Event e;
-	while (_window->pollEvent(e))
-	{
-		_gameScene->ProcessInput(e);
-		_shouldTerminate = _gameScene->ShouldTerminate();
-	}
-}
-
-void Game::Update(const sf::Time & elapsedTime)
-{
-	_gameScene->Update(elapsedTime);
-}
-
-void Game::Render()
-{
-	_window->clear();
-	_gameScene->Render();
-	_window->display();
-}
-
-void Game::PostFrame()
-{
-	_gameScene->PostFrame();
+	_delegate->PostGameLoop();
 }
 
 void Game::GameLoop()
 {
-	const size_t expectedFps = 60;
+	const sf::Int32 expectedFps = _delegate->RequestedFps();
 	const auto expectedTps = sf::milliseconds(1000 / expectedFps);
 
 	const auto deltaTime = sf::microseconds(1000);
@@ -91,20 +26,19 @@ void Game::GameLoop()
 	// Guarantee at least one update before render.
 	sf::Time  timeSinceLastUpdate = deltaTime;
 
-	while (!_shouldTerminate)
+	while (!_delegate->ShouldTerminate())
 	{
 		timeSinceLastUpdate += frameClock.restart();
 
-		PreFrame();
-		HandleInput();
+		_delegate->PreFrame();
+		_delegate->Input();
 		while (timeSinceLastUpdate >= deltaTime)
 		{
 			timeSinceLastUpdate -= deltaTime;
-			Update(deltaTime);
+			_delegate->Update();
 		}
-		
-		Render();
-		PostFrame();
+		_delegate->Render();
+		_delegate->PostFrame();
 
 		const auto elapsedTime = frameClock.getElapsedTime();
 		if (elapsedTime < expectedTps)
