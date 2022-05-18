0eeee71ea853161d6da258ef0e6f5aaba72ff942
Polyakov Pavel <16599808+KOMMYHAP@users.noreply.github.com>
2022-05-18 23:09:19 +0700

    Поднял стандарт до с++20

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -2,7 +2,7 @@ macro(target_enable_cpplatest project)
 	if(MSVC)
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++latest")
 	else()
-		target_compile_features(${project} PRIVATE cxx_std_17)
+		target_compile_features(${project} PRIVATE cxx_std_20)
 	endif()
 endmacro()
 
